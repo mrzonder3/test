562792d2463198cc01e21a3396ef12652870a665
mrzonder3 <84039493+mrzonder3@users.noreply.github.com>
2021-05-12 13:47:56 +0300

    last fix

--- backup.py
+++ backup.py
@@ -6,14 +6,7 @@ backupFolderName = datetime.now().strftime("%Y-%m-%d-%H-%M-%S");
 
 cmd = "pg_basebackup -D /tmp/backup/{0} -U postgres".format(backupFolderName)
 
-if os.system(cmd) == 0:
-    metric = 'Backup has been created successfully'
-    metricCode = 1
-else:
-    metric = 'Backup failed'
-    metricCode = 0
-
 registry = CollectorRegistry()
-g = Gauge('backup_status', metric, registry=registry)
-g.set(metricCode)
+g = Gauge('backup_status', 'Backup Status', registry=registry)
+g.set(os.system(cmd) == 0 if 1 else 0)
 push_to_gateway('pushgateway:9091', job='pg_basebackup', registry=registry)
