533e51edf28554df32a3710da93c817d68b3a3dd
mrzonder3 <84039493+mrzonder3@users.noreply.github.com>
2021-05-12 11:43:11 +0300

    Handle errors

--- backup.py
+++ backup.py
@@ -5,9 +5,13 @@ from prometheus_client import CollectorRegistry, Gauge, push_to_gateway, instanc
 backupFolderName = datetime.now().strftime("%Y-%m-%d-%H-%M-%S");
 
 cmd = "pg_basebackup -D /tmp/backup/{0} -U postgres".format(backupFolderName)
-os.system(cmd)
+
+if os.system(cmd) == 0:
+    metric = 'Backup has been created successfully'
+else:
+    metric = 'Backup failed'
 
 registry = CollectorRegistry()
-g = Gauge('metrics', 'Backup has been created successfully', registry=registry)
+g = Gauge('metrics', metric, registry=registry)
 g.set_to_current_time()
 push_to_gateway('pushgateway:9091', job='pg_basebackup', registry=registry)
