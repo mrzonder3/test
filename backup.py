c162fd77e09349d1652677de1a870107ee8d7925
mrzonder3 <84039493+mrzonder3@users.noreply.github.com>
2021-05-12 13:59:30 +0300

    last last fix

--- backup.py
+++ backup.py
@@ -1,12 +1,14 @@
 import os
 from datetime import datetime
-from prometheus_client import CollectorRegistry, Gauge, push_to_gateway, instance_ip_grouping_key
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 backupFolderName = datetime.now().strftime("%Y-%m-%d-%H-%M-%S");
 
 cmd = "pg_basebackup -D /tmp/backup/{0} -U postgres".format(backupFolderName)
 
+exit_code = os.system(cmd)
+
 registry = CollectorRegistry()
 g = Gauge('backup_status', 'Backup Status', registry=registry)
-g.set(os.system(cmd) == 0 if 1 else 0)
+g.set(1 if exit_code == 0 else 0)
 push_to_gateway('pushgateway:9091', job='pg_basebackup', registry=registry)
